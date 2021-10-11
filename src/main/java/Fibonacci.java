77827b58d99b729b7af5f845bde5bc4cc311008c
Giorgimuf <giorgimuf@gmail.com>
2021-10-11 23:54:47 +0400

    version 1.0

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static long fib(long i) {
+        if (i <= 1) return i;
+        long prevFib = 0, currFib = 1;
+        for (long k = 0; k < i-1; k++){
+            long newFib = prevFib + currFib;
+            prevFib=currFib;
+            currFib=newFib;
+        }
+        return currFib;
+    }
+
+    public static void main(String[] args) {
+        long  i=50;
+
+
+        Scanner sc = new Scanner(System.in);
+        try
+        {
+            System.out.println("Give me index and i'll give you fibonacci number:");
+            i = sc.nextInt();
+        }
+        catch (InputMismatchException exception) {
+            System.out.println("hey man, just give me a number");
+        }
+
+        System.out.println("soo, anyway Fibonacci number by index "+i+" is: "+ fib(i));
+    }
+}
